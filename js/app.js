400d163cf4ffc593b29182c0452037dba3a76c3e
ehsan-afsharnia <iamafsharnia@gmail.com>
2023-01-25 23:52:57 +0330

    Reading values

---
+++ js/app.js
@@ -0,0 +1,72 @@
+// -------------|class|------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//-------------------|listenres|--------------
+
+
+listeners()
+
+function listeners() {
+
+    document.querySelector('#submitBtn').addEventListener('click', search)
+
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+//-------------------|functions|-----------------
+
+function search(e) {
+    e.preventDefault()
+    const newsName = document.querySelector('#news-name').value
+    const country = document.querySelector('#country').value
+    const category = document.querySelector('#category').value
+
+    if (newsName !== '' || country !== '' || category !== '') {
+        console.log('correct');
+    } else {
+        console.log('error');
+    }
+
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
